a9127ae20eb39f21bd0fa9f3b4032e32cb3dcaf4
vanessa.vml.vm@hotmail.com <vanessa.vml.vm@hotmail.com>
2021-10-19 18:51:28 -0300

    first commit

---
+++ CalculoAlunoMedia/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CalculoAlunoMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, media1;
+
+            Console.WriteLine("Digite a primeira nota:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota:");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            media1 = (n1 + n2 + n3) / 3;
+            
+            if (media1 >= 7) 
+            {
+                Console.WriteLine(" Sua média é {0}.Aluno aprovado!", media1);
+            } else
+            {
+                int nota_exame;
+
+                Console.WriteLine("Digite a nota do exame:");
+                nota_exame = Convert.ToInt32(Console.ReadLine());
+
+                int media2; 
+
+                media2 = (media1 + nota_exame) / 2;
+
+                if (media2 >= 5)
+                {
+
+                    Console.WriteLine("Sua média é {0}.Aluno aprovado!", media2);
+                }
+
+                else
+                {
+                    Console.WriteLine("Sua média é {0}.Aluno reprovado: ", media2);
+
+                }
+
+                
+            }
+
+        }
+    }
+}
